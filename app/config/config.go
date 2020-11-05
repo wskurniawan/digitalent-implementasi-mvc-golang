4d35403a4a6081fa2f1fbae6fbbab428b2a0d6a8
wskurniawan <kurniawan.wisnu23@gmail.com>
2020-11-05 07:38:28 +0700

    add tcp to connection string

--- app/config/config.go
+++ app/config/config.go
@@ -9,7 +9,7 @@ import (
 )
 
 func DBInit() *gorm.DB {
-	db, err := gorm.Open(mysql.Open(fmt.Sprintf("%s:%s@%s:%s/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect DB" + err.Error())
 	}
